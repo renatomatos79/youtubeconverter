eabb4ddf2ca70d7c8ff86971b54e6752acd56b97
renato matos <renato.matos79@gmail.com>
2019-12-15 23:00:58 +0000

    download from youtube

---
+++ YoutubeConverter/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace YoutubeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.dlgMp4 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(12, 82);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.BtnDownload_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(525, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(546, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Info";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(12, 48);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(52, 13);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "video title";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(93, 82);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 5;
+            this.btnConvert.Text = "MP4 to MP3";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.BtnConvert_Click);
+            // 
+            // dlgMp4
+            // 
+            this.dlgMp4.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 274);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Youtube Download";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.OpenFileDialog dlgMp4;
+    }
+}
+
---
+++ YoutubeConverter/Form1.cs
@@ -0,0 +1,98 @@
+﻿using NReco.VideoConverter;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using VideoLibrary;
+using YoutubeConverter.Properties;
+using YoutubeExplode;
+
+namespace YoutubeConverter
+{
+    public partial class Form1 : Form
+    {
+        private YoutubeClient client = new YoutubeClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDownload_Click(object sender, EventArgs e)
+        {
+                    
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnDownload.Visible = false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var link = textBox1.Text;
+                lblTitle.Tag = YoutubeClient.ParseVideoId(link);
+                var playlist = client.GetPlaylistAsync(lblTitle.Tag.ToString()).Result;
+                var video = playlist.Videos.First();
+                lblTitle.Text = video.Title;                
+            }
+            catch 
+            {
+                lblTitle.Text = Guid.NewGuid().ToString();
+            }
+            finally
+            {
+                btnDownload.Visible = true;
+            }
+        }
+
+        private void BtnDownload_Click_1(object sender, EventArgs e)
+        {
+            btnDownload.Enabled = false;
+            try
+            {
+                var file = $@"C:\Temp\Download\{lblTitle.Text}.mp4";
+                var streamInfoSet = client.GetVideoMediaStreamInfosAsync(lblTitle.Tag.ToString()).Result.Muxed.FirstOrDefault();
+                // Download stream to file
+                var download = client.DownloadMediaStreamAsync(streamInfoSet, file);
+                while (!download.IsCompleted)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                }
+                MessageBox.Show($"Download Complete {file}");
+            }
+            finally
+            {
+                btnDownload.Enabled = true;
+                btnDownload.Visible = false;
+            }
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            if (dlgMp4.ShowDialog() == DialogResult.OK)
+            {
+                btnConvert.Enabled = false;
+                try
+                {
+                    var output = Path.ChangeExtension(dlgMp4.FileName, "mp3");
+                    ConvertToMp3(dlgMp4.FileName, output);
+                    MessageBox.Show($"Complete!");
+                }
+                finally
+                {
+                    btnConvert.Enabled = true;
+                }
+            }
+        }
+
+        private void ConvertToMp3(string inputFile, string outputFile)
+        {
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+            ffMpeg.ConvertMedia(inputFile, outputFile, Format.mp4);
+        }
+    }
+}
